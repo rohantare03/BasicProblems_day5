57e011d33f83938a58cf7d6f3b15376694b27942
rohantare03 <rohatare123@gmail.com>
2022-05-10 16:25:31 +0530

    [Rohan] Add : Fliping the coin and checking Precentage of heads and tails

---
+++ Basic_Programs/Basic_Programs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Basic_Programs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a  number for a particular problem.\n");
+            Console.WriteLine("1 for Flip coin and finding percentage of heads and tails");
+
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            Program basicprogram = new Program();
+            switch (Problem)
+            {
+                case 1:
+                    FlipCoin coin = new FlipCoin();
+                    coin.Cointoss();
+                    break; 
+            }
+        }
+    }
+}
---
+++ Basic_Programs/Basic_Programs/flipcoin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    internal class FlipCoin 
+    {
+        public void Cointoss()
+        {
+            int Heads = 0;
+            int Tails = 0;
+
+            Console.WriteLine("Enter the number of times you want to flip the coin :");
+            int NumberOfFlips = Convert.ToInt32(Console.ReadLine());
+
+            while (NumberOfFlips <= 0)
+            {
+                Console.WriteLine("Invalid input, Please input a number greater than 0. ");
+                Console.WriteLine("Please enter again:");
+                NumberOfFlips = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < NumberOfFlips; i++)
+            {
+                Random check = new Random();
+                int headortail = check.Next(0, 2);
+
+                if (headortail < 0.5)
+                {
+                    Heads++;
+                }
+                else
+                {
+                    Tails++;
+                }
+            }
+
+            float HeadsPercent = (float)Heads * 100 / NumberOfFlips;
+            float TailsPercent = (float)Tails * 100 / NumberOfFlips;
+
+            Console.WriteLine(HeadsPercent + "% of the times the result was heads.");
+            Console.WriteLine(TailsPercent + "% of the times the result was tails.");
+        }
+    }
+}
