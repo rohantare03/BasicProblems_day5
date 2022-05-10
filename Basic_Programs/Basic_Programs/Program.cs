7601b6d8c33e689bf4ea61087323ed78f4f9558d
rohantare03 <rohatare123@gmail.com>
2022-05-10 16:49:20 +0530

    [Rohan] Add : Checking if the year is leap year or not

--- Basic_Programs/Basic_Programs/Program.cs
+++ Basic_Programs/Basic_Programs/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Enter a  number for a particular problem.\n");
             Console.WriteLine("1 for Flip coin and finding percentage of heads and tails");
+            Console.WriteLine("2 for checking Leap year or not");
 
             int Problem = Convert.ToInt32(Console.ReadLine());
             Program basicprogram = new Program();
@@ -14,6 +15,10 @@
                 case 1:
                     FlipCoin coin = new FlipCoin();
                     coin.Cointoss();
+                    break; 
+                case 2:
+                    leapyear leap = new leapyear();
+                    leap.LeapYear();
                     break; 
             }
         }
---
+++ Basic_Programs/Basic_Programs/leapyear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    internal class leapyear
+    {
+        public void LeapYear()
+        {
+            Console.WriteLine("Enter the year : ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            while (Year < 1000 || Year > 9999)
+            {
+                Console.WriteLine("Enter a four digit number : ");
+                Year = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+            {
+                Console.WriteLine(Year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(Year + " is not a leap year");
+
+            }
+        }
+    }
+}
